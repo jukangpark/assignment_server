2ac69274e1dac69e7cafcf7b760513498f434ea9
jukangpark <skyxxx9339@naver.com>
2023-01-02 14:56:02 +0900

    getProfile 에 verify token 제거

--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -16,7 +16,7 @@ const userRouter = express.Router();
 userRouter.route("/join/:name").post(join); // 유저 생성
 userRouter.route("/login/:name").post(login); // 유저 로그인
 userRouter.route("/myProfile/:name").get(verifyToken, getMyProfile); // 나의 프로필 조회
-userRouter.route("/:id/:name").get(verifyToken, getProfile); // 나의 프로필 조회
+userRouter.route("/:id/:name").get(getProfile); // 모든 사람들 프로필 조회
 userRouter.route("/:id/:name").put(updateProfile); // 유저 프로필 업데이트
 userRouter.route("/:id/:name").delete(deleteProfile); // 유저 프로필 삭제
 userRouter.route("/:id/follow/:name").post(verifyToken, follow); // 해당 유저 팔로우
